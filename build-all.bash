384bf8248cb36191ca96e7c0982371895e95f513
yuantong <zosidev@163.com>
2025-05-21 21:59:35 +0800

    - feat: add build-all.bash to build android,ohos and iOS arm64 dynamic libs with one command

---
+++ build-all.bash
@@ -0,0 +1,3 @@
+bash scripts/build-ios.bash -S source -B build/ios -I output/ios --platform OS64
+bash scripts/build-android.bash -S source -B build/android -I output/android
+bash scripts/build-ohos.bash -S source -B build/ohos -I output/ohos
